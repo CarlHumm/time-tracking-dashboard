22eb4289b7d7bcbda9db4848bbb50ab9d82b1820
Carl Humm <justscarl@gmail.com>
2025-04-17 23:10:16 +0100

    Styling changes, and basic draft JS functionality
    Need to update some styles to match the design, particularly the profile section. Convert to relative units for accessibility

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import './style.scss';
 
 window.addEventListener('DOMContentLoaded', async (e) => {
    
+   const buttons = document.querySelectorAll('.dashboard__control-button');
+   buttons.forEach((button) => {
+    button.addEventListener('click', toggleData);
+   });
+
    const cards = await fetchData('./data.json');
 
    if(!cards) {
@@ -37,7 +42,7 @@ function renderCards(cards) {
 
           Object.entries(card.timeframes).forEach(([range, values]) => {
         statsMarkup += `
-          <div class="card__stats" data-range="${range}">
+          <div class="card__stats ${range === "daily" ? "show" : ""}" data-range="${range}">
             <p class="card__stat card__stat--current">${values.current}hrs</p>
             <p class="card__stat card__stat--previous">Previous - ${values.previous}hrs</p>
           </div>
@@ -46,10 +51,11 @@ function renderCards(cards) {
 
       cardElements.push(`
         <article class="card dashboard__item rounded card--${formatSpaces(card.title)}">
-          <div class="card__inner cushion-2">
+          <div class="card__background"></div>
+          <div class="card__inner rounded cushion-2">
             <div class="card__header">
               <h3 class="card__title">${card.title}</h3>
-              <button class="card__menu-button" aria-label="Options">...</button>
+              <button class="card__menu-button" aria-label="Options"><svg width="21" height="5" xmlns="http://www.w3.org/2000/svg"><path d="M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z" fill="#BBC0FF" fill-rule="evenodd"/></svg></button>
             </div>
             ${statsMarkup}
           </div>
@@ -66,7 +72,25 @@ function formatSpaces(str) {
   return str.replace(/\s+/g, '-').toLowerCase()
 }
 
+function toggleData(e) {
 
+if(e.target.getAttribute('aria-pressed') === "true") {
+  return;
+}
+
+  document.querySelector('[aria-pressed="true"]').setAttribute('aria-pressed', false);
+  e.target.setAttribute('aria-pressed', true);
+
+  let range = e.target.getAttribute('data-range');
+
+  document.querySelectorAll('.card__stats.show').forEach((stat) => {
+    stat.classList.remove('show');
+  })
+
+ document.querySelectorAll(`.card__stats[data-range=${range}]`).forEach((stat) => {
+  stat.classList.add('show');
+ });
+}
 
 
 
